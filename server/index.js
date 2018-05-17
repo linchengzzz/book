332cc317930dd5eab56ec0cd17a45186f6efca39
zhangchenglin <1139489076@qq.com>
2018-05-17 22:12:38 +0800

    9.fixBug.2

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const bodyParse = require('body-parser');
 const app = express();
 const connection = require('./mysql').connection;
 app.listen(8010,()=>{
-    console.log('server start');
+    console.log('server start:8010');
 });
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Origin", "http://localhost");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header('Access-Control-Allow-Credentials', true);
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
